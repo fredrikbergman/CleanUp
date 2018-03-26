cd453d44db39f88f49aa20cc6bfcef1eed68b9db
Fredrik <fr3drik.b3rgman@gmail.com>
2018-03-26 17:28:59 +0200

    Added EF

--- CleanUp/Pages/Index.cshtml.cs
+++ CleanUp/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace CleanUp.Pages
 {
@@ -13,17 +14,22 @@ namespace CleanUp.Pages
         public DateTime Time { get; set; }
         public string DB { get; set; }
 
-        public IConfiguration Configuration { get; set; }
+        private IConfiguration Configuration { get; set; }
+        private ILogger<IndexModel> Logger { get; set; }
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IConfiguration configuration, ILogger<IndexModel> logger)
         {
             Configuration = configuration;
+            Logger = logger;
         }
 
         public void OnGet()
         {
             Time = DateTime.Now;
             DB = Configuration["DbConnection"];
+            Logger.LogInformation("Loggar info");
+            Logger.LogWarning("Loggar varning");
+            Logger.LogError("Loggar error");
         }
     }
 }
--- CleanUp/Startup.cs
+++ CleanUp/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanUp.DB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CleanUp
 {
@@ -23,13 +26,18 @@ namespace CleanUp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging();
+
             services.AddMvc();
+
+            var connection = Configuration["DbConnection"];
+            services.AddDbContext<CleanUpContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var test = Configuration["DbConnection"];
+            
 
             if (env.IsDevelopment())
             {
